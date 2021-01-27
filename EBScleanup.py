03eb39f16b7cc4fc71dd03f5bd5d4a2f742d2e8b
Rajeshgupta123456789 <32204956+Rajeshgupta123456789@users.noreply.github.com>
2021-01-27 10:05:13 -0500

    Create EBScleanup.py
    This is Python script written in 3.7 that deletes the EBS volumes which are in available state.

---
+++ EBScleanup.py
@@ -0,0 +1,36 @@
+import boto3
+
+# Set the global variables
+globalVars  = {}
+
+globalVars['REGION_NAME']           = "eu-west-3"
+
+
+ec2       = boto3.resource('ec2', region_name = globalVars['REGION_NAME'] )
+
+def lambda_handler(event, context):
+
+    deletedVolumes=[]
+
+    # Get all the volumes in the region
+    for vol in ec2.volumes.all():
+        if  vol.state=='available':
+
+            # Check for Tags
+           # if vol.tags is None:
+                vid=vol.id
+                v=ec2.Volume(vol.id)
+                v.delete()
+
+                deletedVolumes.append({'VolumeId': vol.id,'Status':'Delete Initiated'})
+                print "Deleted Volume: {0} for not having Tags".format( vid )
+
+              
+
+    # If no Volumes are deleted, to return consistent json output
+    if not deletedVolumes:
+        deletedVolumes.append({'VolumeId':None,'Status':None})
+
+    # Return the list of status of the snapshots triggered by lambda as list
+    return deletedVolumes
+
